4fdeefd77789f71f889ef190610fe6ba7b2f2701
FeraVolt <fera.volt@gmail.com>
2015-12-06 10:11:44 +0500

    Add Adreno booster

--- system/engine/feradroid.sh
+++ system/engine/feradroid.sh
@@ -10,13 +10,18 @@ $B chmod 644 /system/build.prop
 $B chmod -R 777 /system/engine/gears/*
 
 if [ -e /system/etc/sysctl.conf ]; then
-$B chmod 777 /system/etc/sysctl.conf
-$B sysctl -p
+ $B chmod 777 /system/etc/sysctl.conf
+ $B sysctl -p
 else
-$B touch /system/etc/sysctl.conf
-$B chmod 777 /system/etc/sysctl.conf
+ $B touch /system/etc/sysctl.conf
+ $B chmod 777 /system/etc/sysctl.conf
 fi;
 sync;
+
+if [ ! -h /data/local/tmp/adreno_config.txt ]; then
+ $B chmod 777 /system/engine/assets/adreno_config.txt
+ $B ln -s /system/engine/assets/adreno_config.txt /data/local/tmp/adreno_config.txt
+fi;
 
 $SH /system/engine/gears/001abc.sh
 $SH /system/engine/gears/002def.sh

e94f82cef85528011d69851bfda862d76b121d94
FeraVolt <fera.volt@gmail.com>
2015-12-18 23:10:00 +0500

    More net tweaks && fix kernel gear

--- system/engine/gears/009yza.sh
+++ system/engine/gears/009yza.sh
@@ -7,7 +7,20 @@ $B echo "***Network gear***"
 $B echo " "
 $B echo " "
 
-$B sysctl -e -w net.ipv4.tcp_congestion_control = cubic
+$B sysctl -e -w net.ipv4.tcp_congestion_control=cubic
+$B sysctl -e -w net.ipv4.tcp_rfc1337=1
+$B sysctl -e -w net.ipv4.tcp_window_scaling=1
+$B sysctl -e -w net.ipv4.tcp_sack=1
+$B sysctl -e -w net.ipv4.tcp_fack=1
+$B sysctl -e -w net.ipv4.ip_no_pmtu_disc=0
+$B sysctl -e -w net.ipv4.tcp_timestamps=1
+$B sysctl -e -w net.ipv4.tcp_no_metrics_save=1
+$B sysctl -e -w net.ipv4.tcp_moderate_rcvbuf=1
+$B sysctl -e -w net.ipv4.tcp_synack_retries=2
+$B sysctl -e -w ipv4.tcp_fin_timeout=30
+$B sysctl -e -w net.ipv4.ip_forward=1
+
+
 
 $B echo " "
 $B echo " "

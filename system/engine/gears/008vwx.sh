dd1bf0869b5fc1e2ad8d1445beec84432826058b
FeraVolt <fera.volt@gmail.com>
2015-12-21 02:38:30 +0500

    Be more smarter..

--- system/engine/gears/008vwx.sh
+++ system/engine/gears/008vwx.sh
@@ -33,14 +33,19 @@ $B echo "net.ipv4.tcp_synack_retries=2" >> /system/etc/sysctl.conf
 $B echo "net.ipv4.tcp_fin_timeout=30" >> /system/etc/sysctl.conf
 $B echo "net.ipv4.ip_forward=1" >> /system/etc/sysctl.conf
 
-$B echo "Tuning Android settings.."
-sqlite3 /data/data/com.android.providers.settings/databases/settings.db "INSERT INTO secure (name, value) VALUES ('wifi_country_code', 'JP');"
-wifi_idle_wait=180000
-RV=$(sqlite /data/data/com.android.providers.settings/databases/settings.db "select value from secure where name='wifi_idle_ms'")
-if [ "$RV" = '' ]; then
-sqlite /data/data/com.android.providers.settings/databases/settings.db "insert into secure (name, value) values ('wifi_idle_ms', $wifi_idle_wait )"
+if [ -e /system/xbin/sqlite3 ]; then
+ $B echo "Tuning Android settings.."
+ /system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "INSERT INTO secure (name, value) VALUES ('wifi_country_code', 'JP');"
+ wifi_idle_wait=180000
+ RV=$(/system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "select value from secure where name='wifi_idle_ms'")
+ if [ "$RV" = '' ]; then
+  /system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "insert into secure (name, value) values ('wifi_idle_ms', $wifi_idle_wait )"
+ else
+  /system/xbin/sqlite3 /data/data/com.android.providers.settings/databases/settings.db "update secure set value=$wifi_idle_wait where name='wifi_idle_ms'"
+ fi;
+ sync;
 else
-sqlite /data/data/com.android.providers.settings/databases/settings.db "update secure set value=$wifi_idle_wait where name='wifi_idle_ms'"
+ $B echo "No sqlite binary."
 fi;
 
 setprop wifi.supplicant_scan_interval 180
--- system/engine/gears/010bcd.sh
+++ system/engine/gears/010bcd.sh
@@ -6,166 +6,20 @@ B=/system/engine/bin/busybox
 $B echo "Apps gear***"
 $B echo " "
 $B echo " "
-zipalign="yes"
-$B mount -o remount,rw /data 2>/dev/null
-$B mount -o remount,rw /system 2>/dev/null
-$B mount -o remount,rw "$($B mount | grep system | awk '{print $1,$3}' | sed -n 1p)" 2>/dev/null
-rm /data/fixaligntemp 2>/dev/null
-START=`$B date +%s`
-BEGAN=`date`
-TOTAL=`cat /d*/system/packages.xml | grep -E "^<package.*serId" | wc -l`
-INCREMENT=3
-PROGRESS=0
-PROGRESS_BAR=""
-echo "Start: $BEGAN"
-sync
-cat /d*/system/packages.xml | grep -E "^<package.*serId" | while read pkgline; do
-	if [ ! -f "/data/fixaligntemp" ]; then ALIGNED=0; FAILED=0; ALREADY=0; SKIPPED=0; fi
-	PKGNAME=`echo $pkgline | sed 's%.* name="\(.*\)".*%\1%' | cut -d '"' -f1`
-	CODEPATH=`echo $pkgline | sed 's%.* codePath="\(.*\)".*%\1%' |  cut -d '"' -f1`
-	DATAPATH=/d*/d*/$PKGNAME
-	PKGUID=`echo $pkgline | sed 's%.*serId="\(.*\)".*%\1%' | cut -d '"' -f1`
-	PROGRESS=$(($PROGRESS+1))
-	PERCENT=$(( $PROGRESS * 100 / $TOTAL ))
-	if [ "$PERCENT" -eq "$INCREMENT" ]; then
-		INCREMENT=$(( $INCREMENT + 3 ))
-		PROGRESS_BAR="$PROGRESS_BAR="
-	fi
-	clear
-	echo ""
-	echo -n "                                        >"
-	echo -e "\r       $PROGRESS_BAR>"
-	echo "       \"Fixing..\"                    "
-	echo -n "                                        >"
-	echo -e "\r       $PROGRESS_BAR>"
-	echo ""
-	echo "       Processing Apps - $PERCENT% ($PROGRESS of $TOTAL)"
-	echo " Fixing $PKGNAME..."
-	echo ""
-	if [ -e "$CODEPATH" ]; then
-		if [ "$zipalign" ]; then
-			if [ "$($B basename $CODEPATH )" = "framework-res.apk" ] || [ "$($B basename $CODEPATH )" = "com.htc.resources.apk" ]; then
-				echo " NOT ZipAligning (Problematic) $CODEPATH..."
-				SKIPPED=$(($SKIPPED+1))
-			else
-				/system/engine/bin/zipalign -c 4 $CODEPATH
-				ZIPCHECK=$?
-				if [ "$ZIPCHECK" -eq 1 ]; then
-					echo " ZipAligning $CODEPATH..."
-					/system/engine/bin/zipalign -f 4 $CODEPATH /cache/$($B basename $CODEPATH )
-					rc="$?"
-					if [ "$rc" -eq 0 ]; then
-						if [ -e "/cache/$($B basename $CODEPATH )" ]; then
-							$B cp -f -p /cache/$($B basename $CODEPATH ) $CODEPATH
-							ALIGNED=$(($ALIGNED+1))
-						else
-							echo " ZipAligning $CODEPATH... Failed (No Output File!)"
-							FAILED=$(($FAILED+1))
-						fi
-					else echo "ZipAligning $CODEPATH... Failed (rc: $rc!)"
-						FAILED=$(($FAILED+1))
-					fi
-					if [ -e "/cache/$($B basename $CODEPATH )" ]; then $B rm /cache/$($B basename $CODEPATH ); fi
-				else
-					echo " ZipAlign already completed on $CODEPATH "
-					ALREADY=$(($ALREADY+1))
-				fi
-				echo "$ALIGNED $FAILED $ALREADY $SKIPPED" > /data/fixaligntemp
-			fi
-		fi
-		APPDIR=`$B dirname $CODEPATH`
-		if [ "$APPDIR" = "/system/app" ] || [ "$APPDIR" = "/vendor/app" ] || [ "$APPDIR" = "/system/framework" ]; then
-			$B chown 0 $CODEPATH
-			$B chown :0 $CODEPATH
-			$B chmod 644 $CODEPATH
-		elif [ "$APPDIR" = "/data/app" ]; then
-			$B chown 1000 $CODEPATH
-			$B chown :1000 $CODEPATH
-			$B chmod 644 $CODEPATH
-		elif [ "$APPDIR" = "/data/app-private" ]; then
-			$B chown 1000 $CODEPATH
-			$B chown :$PKGUID $CODEPATH
-			$B chmod 640 $CODEPATH
-		fi
-		if [ -d "$DATAPATH" ]; then
-			$B chmod 755 $DATAPATH
-			$B chown $PKGUID $DATAPATH
-			$B chown :$PKGUID $DATAPATH
-			DIRS=`$B find $DATAPATH -mindepth 1 -type d`
-			for file in $DIRS; do
-				PERM=755
-				NEWUID=$PKGUID
-				NEWGID=$PKGUID
-				FNAME=`$B basename $file`
-				case $FNAME in
-							lib)$B chmod 755 $file
-								NEWUID=1000
-								NEWGID=1000
-								PERM=755;;
-				   shared_prefs)$B chmod 771 $file
-								PERM=660;;
-					  databases)$B chmod 771 $file
-								PERM=660;;
-						  cache)$B chmod 771 $file
-								PERM=600;;
-						  files)$B chmod 771 $file
-								PERM=775;;
-							  *)$B chmod 771 $file
-								PERM=771;;
-				esac
-				$B chown $NEWUID $file
-				$B chown :$NEWGID $file
-				$B find $file -type f -maxdepth 2 ! -perm $PERM -exec $B chmod $PERM {} ';'
-				$B find $file -type f -maxdepth 1 ! -user $NEWUID -exec $B chown $NEWUID {} ';'
-				$B find $file -type f -maxdepth 1 ! -group $NEWGID -exec $B chown :$NEWGID {} ';'
-			done
-		fi
-		echo " Fixed Permissions..."
-	fi 2>/dev/null
-done
-sync
-echo $line
-$B mount -o remount,ro /system 2>/dev/null
-$B mount -o remount,ro `$B mount | grep system | awk '{print $1,$3}' | sed -n 1p` 2>/dev/null
-STOP=`$B date +%s`
-ENDED=`date`
-RUNTIME=`$B expr $STOP - $START`
-HOURS=`$B expr $RUNTIME / 3600`
-REMAINDER=`$B expr $RUNTIME % 3600`
-MINS=`$B expr $REMAINDER / 60`
-SECS=`$B expr $REMAINDER % 60`
-RUNTIME=`$B printf "%02d:%02d:%02d\n" "$HOURS" "$MINS" "$SECS"`
-if [ "$zipalign" ]; then
-	ALIGNED=`awk '{print $1}' /data/fixaligntemp`
-	FAILED=`awk '{print $2}' /data/fixaligntemp`
-	ALREADY=`awk '{print $3}' /data/fixaligntemp`
-	SKIPPED=`awk '{print $4}' /data/fixaligntemp`
-	sleep 1
-	rm /data/fixaligntemp
-	echo " Done ZipAligning ALL data and system APKs..."
-	echo ""
-	sleep 1
- 	echo " $TOTAL Apps were processed!"
- 	echo ""
- 	echo " $ALIGNED Apps were zipaligned..."
-	echo " $FAILED Apps were NOT zipaligned due to error..."
-	echo " $SKIPPED Apps were skipped..."
-	echo " $ALREADY Apps were already zipaligned..."
- 	sleep 1
-	echo ""
-	echo $line
-fi
-echo ""
-sleep 1
-echo " FIXED Permissions For ALL $TOTAL Apps..."
-echo $line
-echo ""
-sleep 1
-echo "      Start Time: $BEGAN"
-echo "       Stop Time: $ENDED"
-echo " Completion Time: $RUNTIME"
-echo ""
-sleep 1
+$B mount -o rw,remount /system;
+for DIR in /system/app /data/app /system/priv-app;
+do
+ cd $DIR || exit
+  for APK in *.apk */*.apk;
+  do
+  /system/engine/bin/zipalign -f 4 "$APK" /data/local/"$APK";
+  if [ -e "/data/local/$APK" ]; then
+   $B cp -f -p "/data/local/$APK" "$APK"
+   $B rm -f "/data/local/$APK";
+  fi;
+ done;
+done;
+$B sleep 1
 $B echo " "
 $B echo " "
 $B echo "***Check***"

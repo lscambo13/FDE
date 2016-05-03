64428892b226261ed66bd106916ea840c99970ae
FeraVolt <fera.volt@gmail.com>
2016-05-03 10:38:31 +0500

    Fix FIX

--- system/engine/fix.sh
+++ system/engine/fix.sh
@@ -1,5 +1,6 @@
 #!/system/bin/sh
-### FeraDroid Engine v19 | By FeraVolt. 2016 ###
+### FeraDroid Engine v20 | By FeraVolt. 2016 ###
+B=/system/engine/bin/busybox
 zipalign="yes"
 clear
 sleep 1
@@ -14,20 +15,16 @@ elif [ ! "`which zipalign`" ]; then
 	echo "Zipalign binary was NOT found."
 	echo ""
 fi 2>/dev/null
-busybox mount -o remount,rw /data 2>/dev/null
-busybox mount -o remount,rw /system 2>/dev/null
-busybox mount -o remount,rw `busybox mount | grep system | awk '{print $1,$3}' | sed -n 1p` 2>/dev/null
+$B mount -o remount,rw /data 2>/dev/null
+$B mount -o remount,rw /system 2>/dev/null
+$B mount -o remount,rw `$B mount | grep system | awk '{print $1,$3}' | sed -n 1p` 2>/dev/null
 rm /data/fixaligntemp 2>/dev/null
-START=`busybox date +%s`
+START=`$B date +%s`
 BEGAN=`date`
 TOTAL=`cat /d*/system/packages.xml | grep -E "^<package.*serId" | wc -l`
 INCREMENT=3
 PROGRESS=0
 PROGRESS_BAR=""
-echo "Start Fix Alignment: $BEGAN"
-if [ "$zipalign" ];
- then echo " Start Wheel Alignment ( \"ZepAlign\" ): $BEGAN" 
-fi
 sync
 cat /d*/system/packages.xml | grep -E "^<package.*serId" | while read pkgline; do
 	if [ ! -f "/data/fixaligntemp" ]; then ALIGNED=0; FAILED=0; ALREADY=0; SKIPPED=0; fi
@@ -50,32 +47,32 @@ cat /d*/system/packages.xml | grep -E "^<package.*serId" | while read pkgline; d
 	echo -e "\r       $PROGRESS_BAR>"
 	echo ""
 	echo "       Processing Apps - $PERCENT% ($PROGRESS of $TOTAL)"
-	echo " Fix Aligning $PKGNAME..."
+	echo " Fix Aligning $PKGNAME"
 	echo ""
 	if [ -e "$CODEPATH" ]; then
 		if [ "$zipalign" ]; then
-			if [ "$(busybox basename $CODEPATH )" = "framework-res.apk" ] || [ "$(busybox basename $CODEPATH )" = "com.htc.resources.apk" ]; then
-				echo " NOT ZipAligning (Problematic) $CODEPATH..."
+			if [ "$($B basename $CODEPATH )" = "framework-res.apk" ] || [ "$($B basename $CODEPATH )" = "com.htc.resources.apk" ]; then
+				echo " NOT ZipAligning (Problematic) $CODEPATH"
 				SKIPPED=$(($SKIPPED+1))
 			else
 				zipalign -c 4 $CODEPATH
 				ZIPCHECK=$?
 				if [ "$ZIPCHECK" -eq 1 ]; then
-					echo " ZipAligning $CODEPATH..."
-					zipalign -f 4 $CODEPATH /cache/$(busybox basename $CODEPATH )
+					echo " ZipAligning $CODEPATH"
+					zipalign -f 4 $CODEPATH /cache/$($B basename $CODEPATH )
 					rc="$?"
 					if [ "$rc" -eq 0 ]; then
-						if [ -e "/cache/$(busybox basename $CODEPATH )" ]; then
-							busybox cp -f -p /cache/$(busybox basename $CODEPATH ) $CODEPATH
+						if [ -e "/cache/$($B basename $CODEPATH )" ]; then
+							$B cp -f -p /cache/$($B basename $CODEPATH ) $CODEPATH
 							ALIGNED=$(($ALIGNED+1))
 						else
-							echo " ZipAligning $CODEPATH... Failed (No Output File!)"
+							echo " ZipAligning $CODEPATH Failed (No Output File!)"
 							FAILED=$(($FAILED+1))
 						fi
-					else echo "ZipAligning $CODEPATH... Failed (rc: $rc!)"
+					else echo "ZipAligning $CODEPATH Failed (rc: $rc!)"
 						FAILED=$(($FAILED+1))
 					fi
-					if [ -e "/cache/$(busybox basename $CODEPATH )" ]; then busybox rm /cache/$(busybox basename $CODEPATH ); fi
+					if [ -e "/cache/$($B basename $CODEPATH )" ]; then $B rm /cache/$($B basename $CODEPATH ); fi
 				else
 					echo " ZipAlign already completed on $CODEPATH "
 					ALREADY=$(($ALREADY+1))
@@ -83,68 +80,66 @@ cat /d*/system/packages.xml | grep -E "^<package.*serId" | while read pkgline; d
 				echo "$ALIGNED $FAILED $ALREADY $SKIPPED" > /data/fixaligntemp
 			fi
 		fi
-		APPDIR=`busybox dirname $CODEPATH`
+		APPDIR=`$B dirname $CODEPATH`
 		if [ "$APPDIR" = "/system/app" ] || [ "$APPDIR" = "/vendor/app" ] || [ "$APPDIR" = "/system/framework" ]; then
-			busybox chown 0 $CODEPATH
-			busybox chown :0 $CODEPATH
-			busybox chmod 644 $CODEPATH
+			$B chown 0 $CODEPATH
+			$B chown :0 $CODEPATH
+			$B chmod 644 $CODEPATH
 		elif [ "$APPDIR" = "/data/app" ]; then
-			busybox chown 1000 $CODEPATH
-			busybox chown :1000 $CODEPATH
-			busybox chmod 644 $CODEPATH
+			$B chown 1000 $CODEPATH
+			$B chown :1000 $CODEPATH
+			$B chmod 644 $CODEPATH
 		elif [ "$APPDIR" = "/data/app-private" ]; then
-			busybox chown 1000 $CODEPATH
-			busybox chown :$PKGUID $CODEPATH
-			busybox chmod 640 $CODEPATH
+			$B chown 1000 $CODEPATH
+			$B chown :$PKGUID $CODEPATH
+			$B chmod 640 $CODEPATH
 		fi
 		if [ -d "$DATAPATH" ]; then
-			busybox chmod 755 $DATAPATH
-			busybox chown $PKGUID $DATAPATH
-			busybox chown :$PKGUID $DATAPATH
-			DIRS=`busybox find $DATAPATH -mindepth 1 -type d`
+			$B chmod 755 $DATAPATH
+			$B chown $PKGUID $DATAPATH
+			$B chown :$PKGUID $DATAPATH
+			DIRS=`$B find $DATAPATH -mindepth 1 -type d`
 			for file in $DIRS; do
 				PERM=755
 				NEWUID=$PKGUID
 				NEWGID=$PKGUID
-				FNAME=`busybox basename $file`
+				FNAME=`$B basename $file`
 				case $FNAME in
-							lib)busybox chmod 755 $file
+							lib)$B chmod 755 $file
 								NEWUID=1000
 								NEWGID=1000
 								PERM=755;;
-				   shared_prefs)busybox chmod 771 $file
+				   shared_prefs)$B chmod 771 $file
 								PERM=660;;
-					  databases)busybox chmod 771 $file
+					  databases)$B chmod 771 $file
 								PERM=660;;
-						  cache)busybox chmod 771 $file
+						  cache)$B chmod 771 $file
 								PERM=600;;
-						  files)busybox chmod 771 $file
+						  files)$B chmod 771 $file
 								PERM=775;;
-							  *)busybox chmod 771 $file
+							  *)$B chmod 771 $file
 								PERM=771;;
 				esac
-				busybox chown $NEWUID $file
-				busybox chown :$NEWGID $file
-				busybox find $file -type f -maxdepth 2 ! -perm $PERM -exec busybox chmod $PERM {} ';'
-				busybox find $file -type f -maxdepth 1 ! -user $NEWUID -exec busybox chown $NEWUID {} ';'
-				busybox find $file -type f -maxdepth 1 ! -group $NEWGID -exec busybox chown :$NEWGID {} ';'
+				$B chown $NEWUID $file
+				$B chown :$NEWGID $file
+				$B find $file -type f -maxdepth 2 ! -perm $PERM -exec $B chmod $PERM {} ';'
+				$B find $file -type f -maxdepth 1 ! -user $NEWUID -exec $B chown $NEWUID {} ';'
+				$B find $file -type f -maxdepth 1 ! -group $NEWGID -exec $B chown :$NEWGID {} ';'
 			done
 		fi
-		echo " Fixed Permissions..."
+		echo " Fixed Permissions"
 	fi 2>/dev/null
 done
 sync
 echo $line
-busybox mount -o remount,ro /system 2>/dev/null
-busybox mount -o remount,ro `busybox mount | grep system | awk '{print $1,$3}' | sed -n 1p` 2>/dev/null
-STOP=`busybox date +%s`
+STOP=`$B date +%s`
 ENDED=`date`
-RUNTIME=`busybox expr $STOP - $START`
-HOURS=`busybox expr $RUNTIME / 3600`
-REMAINDER=`busybox expr $RUNTIME % 3600`
-MINS=`busybox expr $REMAINDER / 60`
-SECS=`busybox expr $REMAINDER % 60`
-RUNTIME=`busybox printf "%02d:%02d:%02d\n" "$HOURS" "$MINS" "$SECS"`
+RUNTIME=`$B expr $STOP - $START`
+HOURS=`$B expr $RUNTIME / 3600`
+REMAINDER=`$B expr $RUNTIME % 3600`
+MINS=`$B expr $REMAINDER / 60`
+SECS=`$B expr $REMAINDER % 60`
+RUNTIME=`$B printf "%02d:%02d:%02d\n" "$HOURS" "$MINS" "$SECS"`
 if [ "$zipalign" ]; then
 	ALIGNED=`awk '{print $1}' /data/fixaligntemp`
 	FAILED=`awk '{print $2}' /data/fixaligntemp`
@@ -152,29 +147,19 @@ if [ "$zipalign" ]; then
 	SKIPPED=`awk '{print $4}' /data/fixaligntemp`
 	sleep 1
 	rm /data/fixaligntemp
-	echo " Done ZipAligning ALL data and system APKs..."
+	echo " Done ZipAligning all data and system apps.."
 	echo ""
 	sleep 1
- 	echo " $TOTAL Apps were processed!"
+ 	echo " $TOTAL Apps were processed"
  	echo ""
- 	echo " $ALIGNED Apps were zipaligned..."
-	echo " $FAILED Apps were NOT zipaligned due to error..."
-	echo " $SKIPPED Apps were skipped..."
-	echo " $ALREADY Apps were already zipaligned..."
+ 	echo " $ALIGNED Apps were zipaligned"
+	echo " $FAILED Apps were NOT zipaligned due to error"
+	echo " $SKIPPED Apps were skipped"
+	echo " $ALREADY Apps were already zipaligned"
  	sleep 1
 	echo ""
 	echo $line
 fi
-echo ""
 sleep 1
-echo " FIXED Permissions For ALL $TOTAL Apps..."
-echo $line
-echo ""
-sleep 1
-echo "      Start Time: $BEGAN"
-echo "       Stop Time: $ENDED"
-echo " Completion Time: $RUNTIME"
-echo ""
-sleep 1
-if [ "$zipalign" ]; then echo "Done!"; fi
+echo " Fixed Permissions For ALL $TOTAL Apps"
 echo ""

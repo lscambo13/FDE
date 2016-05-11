0776b223a3144d6582dbdab454df781f918bafb9
FeraVolt <fera.volt@gmail.com>
2016-05-11 23:46:55 +0500

    Rework sleeper daemon

--- system/engine/sleeper.sh
+++ system/engine/sleeper.sh
@@ -3,18 +3,16 @@
 B=/system/engine/bin/busybox
 TIME=$($B date | $B awk '{ print $4 }')
 $B echo "[$TIME] Sleeper daemon"
-if [ ! "$target_usage" ]; then
- target_usage=0
-fi
+U=$($B top -n1)
+LOAD=$(($B top -bn1 | $B grep "Cpu(s)" | $B sed "s/.*, *\([0-9.]*\)%* id.*/\1/" | $B awk '{print $1}'))
 while true; do
- usage=$($B top -n1)
  for i in $($B pgrep -l '' | $B grep '\<org\.\|\<app\.\|\<com\.\|\<android\.' | $B awk '{print $1}'); do
-  if [ "$(IFS=''; $B echo "$usage" | $B grep "$i" | $B awk '{print $(NF-2)}' | $B cut -d'.' -f1)" -gt "$target_usage" ]; then
+  if [ $LOAD -gt "0" ]; then
    kill -9 "$i"
-  fi
- done
- $B sleep 60
+  fi;
+ done;
+ $B sleep 45
  until [ "$(cat /sys/power/wait_for_fb_sleep)" = "sleeping" ]; do
   $B sleep 12
- done
-done
+ done;
+done;

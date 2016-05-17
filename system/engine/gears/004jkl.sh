9a2fa4e10f7ea9ea3c38d18da12b0341e197cab2
FeraVolt <fera.volt@gmail.com>
2016-05-17 23:30:38 +0500

    Good memory gear update

--- system/engine/gears/004jkl.sh
+++ system/engine/gears/004jkl.sh
@@ -4,7 +4,6 @@ B=/system/engine/bin/busybox
 TIME=$($B date | $B awk '{ print $4 }')
 $B echo "[$TIME] 004 - ***Memory gear***"
 RAM=$($B free -m | $B awk '{ print $2 }' | $B sed -n 2p)
-SWAP=$($B free -m | $B awk '{ print $2 }' | $B sed -n 4p)
 if [ "$RAM" -le "1024" ]; then
  KB=$((RAM*10))
 elif [ "$RAM" -le "2048" ]; then
@@ -14,10 +13,11 @@ else
 fi;
 AA="/sys/block/*"
 BB="/sys/devices/virtual/block/*"
+BDI="/sys/devices/virtual/bdi/*"
 MMC="/sys/block/mmc*"
 MTD="/sys/block/mtd*"
-$B echo "Device has $RAM MB of RAM and $SWAP MB of SWAP/ZRAM"
-$B echo "Calculated readahead parameter is $KB KB"
+SD="/sys/devices/virtual/bdi"
+$B echo "Calculated readahead parameter is $SKB KB"
 if [ -e /mnt/sd-ext ]; then
  $B echo "Trying to mount SD-EXT partition if it exists.."
  $B mount -t ext3 -o rw /dev/block/vold/179:2 /mnt/sd-ext
@@ -49,14 +49,21 @@ if [ -e "${i}"/queue/read_ahead_kb ]; then
  $B echo $KB > "${i}"/bdi/read_ahead_kb
  $B echo 0 > "${i}"/queue/iostats
  $B echo 0 > "${i}"/queue/rotational
- $B echo 1 > "${i}"/queue/nomerges
- $B echo 1 > "${i}"/queue/rq_affinity
+ $B echo 2 > "${i}"/queue/nomerges
+ $B echo 0 > "${i}"/queue/rq_affinity
 fi;
 done;
 for b in $MMC $MTD; do
 if [ -e "${b}"/queue/add_random ]; then
  $B echo "Applying new rnd parameters.."
  $B echo 0 > "${b}"/queue/add_random
+fi;
+done;
+for u in $SD; do
+if [ -e "${u}"/*/read_ahead_kb ]; then
+ $B echo "Applying new SKB parameters.."
+ $B echo $KB > "${u}"/*/read_ahead_kb
+ $B echo $SKB > "${u}"/179*/read_ahead_kb
 fi;
 done;
 CHK=$($B cat /sys/devices/virtual/bdi/179:0/read_ahead_kb)

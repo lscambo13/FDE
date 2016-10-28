9b70b6dbfbdc21caac725ef2fcdb32cf899cc305
FeraVolt <fera.volt@gmail.com>
2016-10-28 17:04:12 +0500

    v0.21-beta7..various fixes.

--- system/engine/feradroid.sh
+++ system/engine/feradroid.sh
@@ -13,13 +13,8 @@ CUR=$($B cat /sys/devices/system/cpu/cpu0/cpufreq/scaling_cur_freq)
 CORES=$($B grep -c 'processor' /proc/cpuinfo)
 LOG=/sdcard/Android/FDE_log.txt
 TIME=$($B date | $B awk '{ print $4 }')
-$B mount -o remount,rw /system
 setprop ro.feralab.engine 21
-if [ "$CORES" -gt "3" ] ; then
- $B sleep 69
-else
- $B sleep 96
-fi;
+$B sleep 96
 $B mount -o remount,rw /system
 $B rm -f $LOG
 $B touch $LOG
@@ -27,8 +22,8 @@ if [ -e $LOG ]; then
  $B echo "LOG - OK"
  CONFIG=/sdcard/Android/FDE_config.txt
 else
- LOG=/storage/emulated/0/Android/FDE_log.txt
- CONFIG=/storage/emulated/0/Android/FDE_config.txt
+ LOG=/data/media/0/Android/FDE_log.txt
+ CONFIG=/data/media/0/Android/FDE_config.txt
 fi;
 $B rm -f $LOG
 $B touch $LOG
@@ -38,7 +33,7 @@ $B chmod 777 $LOG
 $B chmod 777 $CONFIG
 $B echo "### FeraLab ###" > $LOG
 $B echo "" >> $LOG
-$B echo "[$TIME] FeraDroid Engine v0.21snap+" >> $LOG
+$B echo "[$TIME] FeraDroid Engine v0.21-b7" >> $LOG
 $B echo "[$TIME] Firing up.." >> $LOG
 $B echo "[$TIME] Device: $(getprop ro.product.brand) $(getprop ro.product.model)" >> $LOG
 $B echo "[$TIME] Architecture: $ARCH" >> $LOG
@@ -68,7 +63,6 @@ if [ -e /system/engine/prop/firstboot ]; then
   /system/engine/assets/gp
  fi;
 fi;
-service call activity 51 i32 0
 TIME=$($B date | $B awk '{ print $4 }')
 if [ -e $CONFIG ]; then
  $B echo "[$TIME] Loading FDE_config.." >> $LOG
@@ -94,15 +88,15 @@ $B echo "[$TIME] Set SElinux permissive.." >> $LOG
 $B echo "[$TIME] Correcting permissions.." >> $LOG
 $B chmod 644 /system/build.prop
 $B chmod -R 777 /cache/*
-$B chmod -R 755 /system/engine/*
-$B chmod 755 /system/engine/assets/*
-$B chmod 755 /system/engine/gears/*
-$B chmod 755 /system/engine/prop/*
+$B chmod -R 777 /system/engine/*
+$B chmod 777 /system/engine/assets/*
+$B chmod 777 /system/engine/gears/*
+$B chmod 777 /system/engine/prop/*
 sync;
 $B mount -o remount,rw /system
 $B rm -f /system/etc/sysctl.conf
 $B touch /system/etc/sysctl.conf
-$B chmod 755 /system/etc/sysctl.conf
+$B chmod 777 /system/etc/sysctl.conf
 TIME=$($B date | $B awk '{ print $4 }')
 if [ -e /system/engine/prop/ferakernel ]; then
  $B echo "[$TIME] FeraKernel init.." >> $LOG
@@ -128,7 +122,7 @@ fi;
 if [ -e /system/engine/gears/03battery.sh ]; then
  TIME=$($B date | $B awk '{ print $4 }')
  $B echo "[$TIME] Running Battery gear.." >> $LOG
- /system/engine/gears/04battery.sh | $B tee -a $LOG
+ /system/engine/gears/03battery.sh | $B tee -a $LOG
 fi;
 if [ -e /system/engine/gears/04vm.sh ]; then
  TIME=$($B date | $B awk '{ print $4 }')
@@ -170,6 +164,7 @@ if [ -e /system/engine/gears/11ram.sh ]; then
  $B echo "[$TIME] Running RAM gear.." >> $LOG
  /system/engine/gears/11ram.sh | $B tee -a $LOG
 fi;
+service call activity 51 i32 0
 if [ -e /system/engine/gears/end.sh ]; then
  TIME=$($B date | $B awk '{ print $4 }')
  $B echo "[$TIME] END init.." >> $LOG
@@ -185,13 +180,12 @@ fi;
 if [ -e /system/engine/prop/firstboot ]; then
  $B mount -o remount,rw /system
  $B rm -f /system/engine/prop/firstboot
- $B mount -o remount,ro /system
  $B echo "[$TIME] First boot completed." >> $LOG
 fi;
 $B echo 96 > /sys/devices/virtual/timed_output/vibrator/enable
-$B sleep 0.5
+$B sleep 0.3
 $B echo 96 > /sys/devices/virtual/timed_output/vibrator/enable
-$B sleep 0.5
+$B sleep 0.3
 $B echo 96 > /sys/devices/virtual/timed_output/vibrator/enable
 TIME=$($B date | $B awk '{ print $4 }')
 $B echo "[$TIME] FDE status - OK" >> $LOG

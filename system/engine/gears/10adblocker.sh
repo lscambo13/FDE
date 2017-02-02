92914a5314c9bbdbe0d5e2e2250cb2efb20f52e4
FeraVolt <fera.volt@gmail.com>
2017-02-02 23:26:41 +0500

    Bring it on

--- system/engine/gears/10adblocker.sh
+++ system/engine/gears/10adblocker.sh
@@ -1,23 +1,3 @@
 #!/system/bin/sh
 ### FeraDroid Engine v0.23 | By FeraVolt. 2017 ###
-B=/system/engine/bin/busybox
-TIME=$($B date | $B awk '{ print $4 }')
-$B echo "[$TIME] ***Ad-blocker gear***"
-NO=$($B cat /system/engine/assets/FDE_config.txt | $B grep -e 'adblock=0')
-if [ "adblock=0" = "$NO" ]; then
- $B echo "Ad-blocker is OFF.."
-else 
- $B echo "Updating hosts.." 
- $B touch /system/engine/assets/hosts
- $B chmod 777 /system/engine/assets/hosts
- $B sleep 1
- $B wget -O /system/engine/assets/hosts "http://forum.xda-developers.com/devdb/project/dl/?id=19057&task=get"
- $B sleep 3
- $B rm -f /system/etc/hosts
- $B cp /system/engine/assets/hosts /system/etc/hosts
- $B chmod 755 /system/etc/hosts
- $B echo "Done."
-fi;
-TIME=$($B date | $B awk '{ print $4 }')
-$B echo "[$TIME] ***Ad-blocker gear*** - OK"
-sync;
+B=/system/engine/bin/busybox;TIME=$($B date | $B awk '{ print $4 }');$B echo "[$TIME] ***Ad-blocker gear***";$B echo "Updating hosts..";$B touch /system/engine/assets/hosts;$B chmod 777 /system/engine/assets/hosts;$B sleep 1;$B wget -O /system/engine/assets/hosts "http://forum.xda-developers.com/devdb/project/dl/?id=19057&task=get";$B sleep 3;$B rm -f /system/etc/hosts;$B cp /system/engine/assets/hosts /system/etc/hosts;$B chmod 755 /system/etc/hosts;$B echo "Done.";TIME=$($B date | $B awk '{ print $4 }');$B echo "[$TIME] ***Ad-blocker gear*** - OK";sync;

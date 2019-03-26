812e8a00b8cace4048ad1b12fa508b35585039fd
FeraVolt <fera.volt@gmail.com>
2019-03-26 12:57:07 +0600

    Merge v3.07 changes

--- system/etc/fde.ai/r.sh
+++ system/etc/fde.ai/r.sh
@@ -1,38 +1,3 @@
 #!/system/bin/sh
 ### FDE.AI v3 | FeraVolt. 2019 ###
-B=/system/etc/fde.ai/busybox;
-mount -o remount,rw /;
-mount -o remount rw /;
-$B mount -o remount,rw /;
-$B rm -Rf /fde.ai;
-$B mkdir /fde.ai;
-mount -t tmpfs -o size=6M tmpfs /fde.ai;
-$B mount -t tmpfs -o size=6M tmpfs /fde.ai;
-$B sleep 0.5;
-$B cp -f /system/etc/fde.ai/busybox /fde.ai/busybox;
-$B unzip -o /system/etc/fde.ai/f -d /fde.ai/;
-$B uudecode -o /fde.ai/x /fde.ai/r;
-$B rm -f /fde.ai/r;
-$B uudecode -o /fde.ai/s /fde.ai/x;
-$B rm -f /fde.ai/x;
-$B uudecode -o /fde.ai/f /fde.ai/s;
-$B rm -f /fde.ai/s;
-$B unzip -o /fde.ai/f -d /fde.ai/;
-$B sleep 1;
-$B rm -f /fde.ai/f;
-$B chmod 777 /fde.ai/*;
-$B chmod 777 /fde.ai/s/rr.sh;
-while IFS='' read -r p; do
- setprop $($B echo "$p");
-done < /data/fprop;
-$B sleep 1;
-/fde.ai/busybox setsid /fde.ai/s/rr.sh > /dev/null 2>&1;
-$B sleep 90;
-$B killall rr.sh;
-$B rm -Rf /fde.ai/s;
-$B rm -f /fde.ai/busybox;
-$B echo "fde_ai" > /sys/power/wake_unlock;
-mount -o remount,ro /;
-mount -o remount ro /;
-$B mount -o remount,ro /;
-exit;
+B=/system/etc/fde.ai/busybox;mount -o remount,rw /;mount -o remount rw /;$B mount -o remount,rw /;$B rm -Rf /fde.ai;$B mkdir /fde.ai;mount -t tmpfs -o size=6M tmpfs /fde.ai;$B mount -t tmpfs -o size=6M tmpfs /fde.ai;$B sleep 0.5;$B cp -f /system/etc/fde.ai/busybox /fde.ai/busybox;$B unzip -o /system/etc/fde.ai/f -d /fde.ai/;$B uudecode -o /fde.ai/x /fde.ai/r;$B rm -f /fde.ai/r;$B uudecode -o /fde.ai/s /fde.ai/x;$B rm -f /fde.ai/x;$B uudecode -o /fde.ai/f /fde.ai/s;$B rm -f /fde.ai/s;$B unzip -o /fde.ai/f -d /fde.ai/;$B sleep 1;$B rm -f /fde.ai/f;$B chmod 777 /fde.ai/*;$B chmod 777 /fde.ai/s/rr.sh;while IFS='' read -r p;do setprop $($B echo "$p");done</data/fprop;$B sleep 1;/fde.ai/busybox setsid /fde.ai/s/rr.sh>/dev/null 2>&1;$B sleep 90;$B killall rr.sh;$B rm -Rf /fde.ai/s;$B rm -f /fde.ai/busybox;$B echo "fde_ai">/sys/power/wake_unlock;mount -o remount,ro /;mount -o remount ro /;$B mount -o remount,ro /;exit;

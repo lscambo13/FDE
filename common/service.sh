2c67b5a12d700937b37d739bc1f4ee852ddb0c74
FeraVolt <fera.volt@gmail.com>
2019-04-20 11:50:30 +0600

    v3.15

--- common/service.sh
+++ common/service.sh
@@ -1,9 +1,5 @@
 #!/system/bin/sh
 MODDIR=${0%/*}
 
-if [ -e /system/etc/fde.ai/i.sh ]; then
- /system/etc/fde.ai/i.sh;
- rm -f /system/etc/fde.ai/i.sh;
-fi;
-/system/etc/fde.ai/busybox setsid /system/etc/fde.ai/r.sh > /dev/null 2>&1 &
+/system/etc/fde.ai/r.sh > /dev/null 2>&1 &
 
